6e242f71160098e844bb07b143e0a7eed759ef73
aimarsg <aimar.s.737@gmail.com>
2024-12-04 15:03:02 +0100

    emission probs function finished

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json, glob, os
 from pathlib import Path
 
+
 def count_occurrences(corpus):
     """
     This function counts the occurrences of each
@@ -25,21 +26,30 @@ def count_occurrences(corpus):
 
                 for line in f:
                     prev_tag, word = tag, ''
-                    if line == '\n':tag = '<EOL>'
-                    elif line[0] == '#': tag = '<BOL>'
-                    else: w_id, word, lemma, tag, _, _, _, tag2, _, _ = line.split('\t')
+                    if line == '\n':
+                        tag = '<EOL>'
+                    elif line[0] == '#':
+                        tag = '<BOL>'
+                    else:
+                        w_id, word, lemma, tag, _, _, _, tag2, _, _ = line.split('\t')
 
                     if not (tag == '<BOL>' and prev_tag == '<BOL>'):
-                        if tag not in counts['tags']: counts['tags'][tag] = 1
-                        else: counts['tags'][tag] += 1
+                        if tag not in counts['tags']:
+                            counts['tags'][tag] = 1
+                        else:
+                            counts['tags'][tag] += 1
 
                         if not (tag == '<BOL>' and prev_tag == '<EOL>'):
-                            if f'{prev_tag}, {tag}' not in counts['transitions']: counts['transitions'][f'{prev_tag}, {tag}'] = 1
-                            else: counts['transitions'][f'{prev_tag}, {tag}'] += 1
+                            if f'{prev_tag}, {tag}' not in counts['transitions']:
+                                counts['transitions'][f'{prev_tag}, {tag}'] = 1
+                            else:
+                                counts['transitions'][f'{prev_tag}, {tag}'] += 1
 
                         if word != '':
-                            if f'{tag}, {word}' not in counts['emissions']: counts['emissions'][f'{tag}, {word}'] = 1
-                            else: counts['emissions'][f'{tag}, {word}'] += 1
+                            if f'{tag}, {word}' not in counts['emissions']:
+                                counts['emissions'][f'{tag}, {word}'] = 1
+                            else:
+                                counts['emissions'][f'{tag}, {word}'] += 1
 
             total_counts[lang] = counts
 
@@ -49,38 +59,49 @@ def count_occurrences(corpus):
     return total_counts
 
 
-
-def calculate_emission_probs(tagprobs, wordtagprobs):
+def calculate_emission_probs(tag_counts: dict, tag_word_counts: dict):
     """
     Calculate the emission probabilities and saves it on a file if the file doesn't exist
-    :param tagprobs: json of the probabilities of the tags
-    :param wordtagprobs: json of probabilities of a word given the tag
+    :param tag_counts: json of the counts of the tags
+    :param tag_word_counts: json of counts of a word given the tag
     :return: emission probabilites
     """
 
+    emission_probs = {}
+    # iterate over tag_word pairs to find the probability of each
+    for key, counts in tag_word_counts.items():
+        tag, word = key.split(', ')  # Ex. "NOUN, oro"
+        count_of_tag = tag_counts[tag]
+        prob = counts/count_of_tag  # counts(tag, word)/counts(tag)
+        emission_probs[key] = prob
+
+    with open("emission_mat.json", "w") as archivo:
+        json.dump(emission_probs, archivo, ensure_ascii=False, indent=4)
+    return emission_probs
 
 
 def calculate_transition_probs(tag_counts: dict, tag_tag_counts: dict):
     """
     Calculates the transition probs and saves it on a file if the file doesn't exist
-    :param tagprobs: json of prob of tags
-    :param tagtagprobs: json of prob of tagtag
+    :param tag_counts: json of counts of tags
+    :param tag_tag_counts: json of counts of tagtag
     :return: transition probabilities for that tag
     """
-    #creamos matriz de transiciones
+    # creamos matriz de transiciones
     trans_mat = {}
-    #recorrer todas las posibles tagtagprobs
-    for tag1_tag2,counts in tag_tag_counts.items():
-        #print(tag1_tag2, counts)
+    # recorrer todas las posibles tagtagprobs
+    for tag1_tag2, counts in tag_tag_counts.items():
+        # print(tag1_tag2, counts)
         prev_tag, current_tag = tag1_tag2.split(", ")
         prev_tag_count = tag_counts.get(f"{prev_tag}")
 
-        prob = counts/prev_tag_count
+        prob = counts / prev_tag_count
         trans_mat[tag1_tag2] = prob
 
     with open("trans_mat.json", "w") as archivo:
         json.dump(trans_mat, archivo, ensure_ascii=False, indent=4)
     return trans_mat
+
 
 def predict_tags(sentence, trans_mat, emiss_mat):
     """
@@ -93,9 +114,11 @@ def predict_tags(sentence, trans_mat, emiss_mat):
 
     pass
 
+
 def main():
     total_counts = count_occurrences(Path('UD-Data'))
     trans_mat = calculate_transition_probs(total_counts["English"]["tags"], total_counts["English"]["transitions"])
+    emission_mat = calculate_emission_probs(total_counts["English"]["tags"], total_counts["English"]["emissions"])
 
 
 if __name__ == "__main__":
