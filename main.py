f34bec390c9a4cc7acd886485b54619bf81ace66
Maits27 <maitiurlo@gmail.com>
2024-12-04 10:08:36 +0100

    Counts

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json, glob, os
+from pathlib import Path
 
 def count_occurrences(corpus):
     """
@@ -9,7 +11,39 @@ def count_occurrences(corpus):
     :param corpus: all the corpus
     :return: returns a json with the counts ({tags:{...}, ...})
     """
-    pass
+    total_counts = {'English': {}, 'Spanish': {}}
+    output_path = Path('UD-Data/data/counts.json')
+
+    if output_path.exists():
+        with open(output_path, 'r', encoding='utf-8') as f:
+            total_counts = json.load(f)
+    else:
+        for lang in total_counts:
+            counts = {'transitions': {}, 'emissions': {}, 'tags': {'*': 1}}
+            with open(glob.glob(os.path.join(f'{corpus}/{lang}/', '*train.conllu'))[0], 'r', encoding='utf-8') as f:
+                tag = '<BOL>'
+                for line in f:
+                    prev_tag, word = tag, ''
+                    if line == '\n':tag = '<EOL>'
+                    elif line[0] == '#': tag = '<BOL>'
+                    else: w_id, word, lemma, tag, _, _, _, tag2, _, _ = line.split('\t')
+
+                    if not (tag == '<BOL>' and prev_tag == '<BOL>'):
+                        if tag not in counts['tags']: counts['tags'][tag] = 1
+                        else: counts['tags'][tag] += 1
+                        if not (tag == '<BOL>' and prev_tag == '<EOL>'):
+                            if f'{prev_tag}, {tag}' not in counts['transitions']: counts['transitions'][f'{prev_tag}, {tag}'] = 1
+                            else: counts['transitions'][f'{prev_tag}, {tag}'] += 1
+                        if word != '':
+                            if f'{tag}, {word}' not in counts['emissions']: counts['emissions'][f'{tag}, {word}'] = 1
+                            else: counts['emissions'][f'{tag}, {word}'] += 1
+            total_counts[lang] = counts
+
+        with open(output_path, 'w', encoding='utf-8') as output_file:
+            json.dump(total_counts, output_file, indent=4)
+
+    return total_counts
+
 
 
 def calculate_emission_probs(tagprobs, wordtagprobs):
@@ -33,7 +67,7 @@ def calculate_transition_probs(tagprobs, tagtagprobs):
 
 
 def main():
-    pass
+    count_occurrences(Path('UD-Data'))
 
 
 if __name__ == "__main__":
