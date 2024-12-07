4c051d7107d12349f4ddefc1eb456bd4dc327799
AdrianCuadron <cuadron11@gmail.com>
2024-12-07 14:10:29 +0100

    Separate different languages and some improvements

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import json, glob, os
 from pathlib import Path
 
-def stablish_unk_words(lines, threshold=2):
+def establish_unk_words(lines, threshold=2):
     """
-    This function stablishes the words that will be considered as unknown
+    This function establishes the words that will be considered as unknown
     :param lines: all the lines of the corpus
     :param threshold: threshold to consider a word as unknown
     :return: a list with the words that will be considered as unknown
@@ -45,7 +45,7 @@ def count_occurrences(corpus):
         for lang in total_counts:
             counts = {'transitions': {}, 'emissions': {}, 'tags': {'<BOL>': 1}}
             with open(glob.glob(os.path.join(f'{corpus}/{lang}/', '*train.conllu'))[0], 'r', encoding='utf-8') as f:
-                unk_words = stablish_unk_words(f.readlines())
+                unk_words = establish_unk_words(f.readlines())
             with open(glob.glob(os.path.join(f'{corpus}/{lang}/', '*train.conllu'))[0], 'r', encoding='utf-8') as f:
                 tag = '<BOL>'
                 for line in f:
@@ -58,6 +58,7 @@ def count_occurrences(corpus):
                         w_id, word, lemma, tag, _, _, _, tag2, _, _ = line.split('\t')
                         if word in unk_words:
                             word = '<UNK>'
+
                     if not (tag == '<BOL>' and prev_tag == '<BOL>'):
                         if tag not in counts['tags']:
                             counts['tags'][tag] = 1
@@ -84,11 +85,12 @@ def count_occurrences(corpus):
     return total_counts
 
 
-def calculate_emission_probs(tag_counts: dict, tag_word_counts: dict):
+def calculate_emission_probs(tag_counts: dict, tag_word_counts: dict, lang: str):
     """
     Calculate the emission probabilities and saves it on a file if the file doesn't exist
     :param tag_counts: json of the counts of the tags
     :param tag_word_counts: json of counts of a word given the tag
+    :param lang: language
     :return: emission probabilites
     """
 
@@ -100,16 +102,17 @@ def calculate_emission_probs(tag_counts: dict, tag_word_counts: dict):
         prob = counts/count_of_tag  # counts(tag, word)/counts(tag)
         emission_probs[key] = prob
 
-    with open("data/emission_mat.json", "w") as archivo:
+    with open(f"data/{lang}_emission_mat.json", "w") as archivo:
         json.dump(emission_probs, archivo, ensure_ascii=False, indent=4)
     return emission_probs
 
 
-def calculate_transition_probs(tag_counts: dict, tag_tag_counts: dict):
+def calculate_transition_probs(tag_counts: dict, tag_tag_counts: dict, lang: str):
     """
     Calculates the transition probs and saves it on a file if the file doesn't exist
     :param tag_counts: json of counts of tags
     :param tag_tag_counts: json of counts of tagtag
+    :param lang: language
     :return: transition probabilities for that tag
     """
     # creamos matriz de transiciones
@@ -123,7 +126,7 @@ def calculate_transition_probs(tag_counts: dict, tag_tag_counts: dict):
         prob = counts / prev_tag_count
         trans_mat[tag1_tag2] = prob
 
-    with open("data/trans_mat.json", "w") as archivo:
+    with open(f"data/{lang}_trans_mat.json", "w") as archivo:
         json.dump(trans_mat, archivo, ensure_ascii=False, indent=4)
     return trans_mat
 
@@ -151,7 +154,7 @@ def evaluate_model(input_path, trans_mat, emiss_mat, all_tags, lang='English'):
                 w_id, word, lemma, tag, _, _, _, tag2, _, _ = line.split('\t')
                 sentence += f'{word} '
                 tags.append(tag)
-    with open('output/predictions.jsonl', 'w', encoding='utf-8') as output_file:
+    with open(f'output/{lang}_predictions.jsonl', 'w', encoding='utf-8') as output_file:
         for p in predictions:
             output_file.write(json.dumps(p, ensure_ascii=False, indent=4) + '\n')
 
@@ -192,13 +195,42 @@ def predict_tags(sentence, trans_mat, emiss_mat, tags, lang='English'):
     return result
 
 
+def predict_examples():
+    #ENGLISH
+
+    for lang in ['English',"Spanish"]:
+        with open(f"data/{lang}_emission_mat.json", "r", encoding="utf-8") as archivo:
+            em = json.loads(archivo.read())
+        with open(f"data/{lang}_trans_mat.json", "r") as archivo:
+            tm = json.loads(archivo.read())
+        with open("data/counts.json", "r") as archivo:
+            counts = json.loads(archivo.read())
+        sentence = input(f"Write a sentence in {lang} or press ENTER to default one: ")
+        if sentence == '':
+            if lang == 'English':
+                sentence = "i would like the cheapest flight from pittsburgh to atlanta leaving april twenty fifth and returning may sixth"
+            else:
+                sentence = "La intérprete de No me importa nada llega mañana , a las diez de la noche , al a el Palau ."
+        res = predict_tags(sentence, tm, em, counts[lang]["tags"].keys())
+        words = sentence.split()  # Separate in words the sentence
+        tags = res[1:-1]
+        for word, tag in zip(words, tags):
+            print(f"{word:<15}{tag}")
+
+
 def main():
+
+    # CREATE COUNTS FOR EACH LANGUAGE
     total_counts = count_occurrences(Path('UD-Data'))
-    trans_mat = calculate_transition_probs(total_counts["English"]["tags"], total_counts["English"]["transitions"])
-    emission_mat = calculate_emission_probs(total_counts["English"]["tags"], total_counts["English"]["emissions"])
-    res = predict_tags('i love your cat so much', trans_mat, emission_mat, total_counts["English"]["tags"].keys())
-    print(res)
-    evaluate_model(Path('UD-Data'), trans_mat, emission_mat, total_counts["English"]["tags"].keys())
+
+    # TRAIN and Evaluate each language
+    for lang in ["English", "Spanish"]:
+        trans_mat = calculate_transition_probs(total_counts[lang]["tags"], total_counts[lang]["transitions"], lang)
+        emission_mat = calculate_emission_probs(total_counts[lang]["tags"], total_counts[lang]["emissions"], lang)
+
+        evaluate_model(Path('UD-Data'), trans_mat, emission_mat, total_counts[lang]["tags"].keys(), lang)
+
+    predict_examples()
 
 
 if __name__ == "__main__":
